10b7094ce33d531977af5e252898f16a1c1a964a
Balaji R <146870775+Balaji-R-05@users.noreply.github.com>
2024-08-02 23:42:30 +0530

    Delete House Price Prediction using Linear Regression/main.py

--- House Price Prediction using Linear Regression/main.py
+++
@@ -1,9 +0,0 @@
-#Importing necessary libraries and modules
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model import LinearRegression
-
-# Loading dataset
-dataset = pd.read_csv("dataset.csv")
-
-
